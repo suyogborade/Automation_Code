0d53222c566ca8956d9a3437ccfdda8510417335
suyogborade <suyog142000@gmail.com>
2024-02-20 12:01:18 +0530

    git commit

--- src/test/java/Selenium_Java/Authenticate_PopUp.java
+++ src/test/java/Selenium_Java/Authenticate_PopUp.java
@@ -2,13 +2,13 @@ package Selenium_Java;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
+
 
 public class Authenticate_PopUp {
 
 	public static void main(String[] args) {
 		
-		WebDriver driver = new SafariDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
 		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
