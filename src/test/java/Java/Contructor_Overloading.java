1a5100db85e9cb5c9470ecf8f28305d7dfdd7773
suyogborade <suyog142000@gmail.com>
2024-04-12 10:26:42 +0530

    Selenium Commit

--- src/test/java/Java/Contructor_Overloading.java
+++ src/test/java/Java/Contructor_Overloading.java
@@ -2,46 +2,40 @@ package Java;
 
 public class Contructor_Overloading {
 
-	int x , y;
-	
-	double d; 
-	
-	Contructor_Overloading()
-	{
-		x=10;
-		y=200;
-	}	
-	
-	Contructor_Overloading(int a, int b)
-	{
-		x=a;
-		 y=b;
-		
+	int x, y;
+
+	double d;
+
+	Contructor_Overloading() {
+		x = 10;
+		y = 200;
 	}
-	
-	Contructor_Overloading(int a, double b)
-	{
-		 x =a;
-		 d= b;
+
+	Contructor_Overloading(int a, int b) {
+		x = a;
+		y = b;
+
 	}
-	
-	
-	Contructor_Overloading( double b, int a)
-	{
-		 x =a;
-		 d= b;
+
+	Contructor_Overloading(int a, double b) {
+		x = a;
+		d = b;
 	}
-	
-	void displayData() 
-	{
-	System.out.println(x);
-	System.out.println(y);
-	System.out.println(d);
+
+	Contructor_Overloading(double b, int a) {
+		x = a;
+		d = b;
 	}
-	
+
+	void displayData() {
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(d);
+	}
+
 	public static void main(String[] args) {
-		
-		Contructor_Overloading cdo = new Contructor_Overloading(10.230,2090);
+
+		Contructor_Overloading cdo = new Contructor_Overloading(10.230, 2090);
 		cdo.displayData();
 
 	}
---
+++ src/test/java/Selenium_Java/DatePicker.java
@@ -0,0 +1,67 @@
+package Selenium_Java;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DatePicker {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/datepicker/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+
+		driver.switchTo().frame(0);
+
+		// Approach 1
+		// driver.findElement(By.className("hasDatepicker")).sendKeys("07/17/2024");
+
+		// Approach 2
+		String year = "2019";
+		String month = "May";
+		String date = "10";
+		driver.findElement(By.xpath("//input[@id='datepicker']")).click();
+
+		while (true) {
+			String mon = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+			String yr = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+
+			if (mon.equals(month) && yr.equals(year)) {
+				break;
+			}
+
+			//driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click(); //future data
+			driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-w']")).click(); //past date
+		}
+
+		List<WebElement> alldate = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+
+	/*	for (WebElement dt : alldate) {
+			if (dt.getText().equals(date)) 
+			{
+				dt.click();
+				break;
+			}
+
+		}       */
+		
+		for(int i=0; i<alldate.size(); i++)
+		{
+			if(alldate.get(i).getText().equals(date))
+			{
+				alldate.get(i).click();
+				break;
+			}
+		}
+		
+		
+		//driver.close();
+	}
+
+}
