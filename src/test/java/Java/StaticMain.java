8725d29b8426277c556ad2018c8b9615f6949cd9
suyogborade <suyog142000@gmail.com>
2024-04-25 11:34:32 +0530

    Java commit

---
+++ src/test/java/Java/StaticMain.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class StaticMain {
+
+public static void main(String[] args) {
+		
+		System.out.println(StaticExample.a);
+
+		StaticExample.m1();
+		
+		StaticExample se = new StaticExample();
+		System.out.println(se.b);
+		se.m2();
+		
+	}
+	
+	
+}
